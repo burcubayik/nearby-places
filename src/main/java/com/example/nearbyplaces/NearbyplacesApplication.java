fcb50661a88bfaf23d67085dd253782ffe51983c
Burcu Bayık <burcu.bayik98@gmail.com>
2024-08-06 23:06:37 +0300

    Initial Commit

---
+++ src/main/java/com/example/nearbyplaces/NearbyplacesApplication.java
@@ -0,0 +1,13 @@
+package com.example.nearbyplaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearbyplacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NearbyplacesApplication.class, args);
+	}
+
+}
