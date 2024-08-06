bcdca042d6fe104b91993b768973b6a3ce7721fb
Burcu Bayık <burcu.bayik98@gmail.com>
2024-08-06 23:34:56 +0300

    feat(place): create Place entity class

---
+++ src/main/java/com/example/nearbyplaces/entity/Place.java
@@ -0,0 +1,22 @@
+package com.example.nearbyplaces.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+//@Table(name="PLACES")
+public class Place {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    private String id;
+    private double latitude;
+    private double longitude;
+    private double radius;
+}
