de3fc07a121b7941a25bce4165299d4007c2d982
Burcu Bayık <burcu.bayik98@gmail.com>
2024-08-13 23:18:06 +0300

    Refactor Place: Repository

--- src/main/java/com/example/nearbyplaces/repository/PlaceRepository.java
+++ src/main/java/com/example/nearbyplaces/repository/PlaceRepository.java
@@ -3,9 +3,12 @@ package com.example.nearbyplaces.repository;
 import com.example.nearbyplaces.dto.PlaceDto;
 import com.example.nearbyplaces.entity.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public interface PlaceRepository extends JpaRepository<Place,String> {
-   List<Place>  findByLatitudeAndAndLongitudeAndRadius(PlaceDto placeDto);
+   Optional<Place> findByLatitudeAndAndLongitudeAndRadius(double latitude, double longitude, double radius);;
 }
